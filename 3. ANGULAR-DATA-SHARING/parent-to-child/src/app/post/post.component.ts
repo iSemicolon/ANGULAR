f185c0dd8b00d017da7b21b0434c78d604ea4256
iSemicolon <palash.samanta7497@gmail.com>
2023-09-29 16:55:56 +0530

    data sharing: Input Decorator

---
+++ 3. ANGULAR-DATA-SHARING/parent-to-child/src/app/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() fromParent:string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
